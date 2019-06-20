ed416606c17a7a6909fd3083f304c16b2a0920ee
Sergio De Bei <sergio@festina.xxx>
2019-06-20 17:11:35 +0200

    Add tooltip on warning

--- src/js/4-global/onDomReady.js
+++ src/js/4-global/onDomReady.js
@@ -14,28 +14,57 @@ $(document).on("ready", function () {
         scroll >= 50 ? body.addClass("page__scrolling") : body.removeClass("page__scrolling");
     });
 
-    // Toggle sheet function
+    // Toggle sheet header function
     var sheet__header = $(".sheet__header");
     sheet__header.on('click', function() {
         $(this).parent().toggleClass('sheet__container--open');
     });
 
+    function updateCounter(){
+        var counter_people = $('.sheet__user').length;
+        var counter_people_present = $('.sheet__user--present').length;
+        // console.log(counter_people, counter_people_present);
+        var span_fraction = $(".sheet__summary__number__counter");
+        span_fraction.empty().text(counter_people_present + '/' + counter_people);
+    }
+
+    // Toggle sheet user function
+    var sheet__user = $(".sheet__user");
+    sheet__user.on('click', function() {
+        console.log('click');
+        $(this).toggleClass('sheet__user--present');
+        updateCounter();
+    });
+
     // Parse json file to create list of users in the sheet
     var sheet__users = $('#sheet__users');
     $.getJSON('./data/data.json', function (data) {
-        // console.log(data, data.length);
+        console.log(data, data.length);
         sheet__users.empty();
 
         if(data.length) {
-            var counter = 0;
-            var counter_present = 0;
             $.each(data,function(index, user){
 
                 var user_present = (user.present) ? ' sheet__user--present' : '';
-                var user_warning = (user.warning) ? ' sheet__user--warning--' + user.warning : '';
+                // var user_warning = (user.warning) ? ' sheet__user--warning--' + user.warning : '';
+                var user_warning = '';
+                var user_warning_tooltip = ' sheet__user--warning__tooltip';
+                var user_warning_tooltip__text = '';
+
+                if(user.balance >= 10) {
+                    user_warning_tooltip = '';
+                }
+                else if((user.balance < 10) && (user.balance > 0)){
+                    user_warning = ' sheet__user--warning--yellow';
+                    user_warning_tooltip__text = 'Almost empty';
+                }
+                else{
+                    user_warning = ' sheet__user--warning--red';
+                    user_warning_tooltip__text = 'Empty';
+                }
 
                 var content = '' +
-                    '<div class="sheet__user' + user_present + user_warning + '" id=user_' + user.userId + '>' +
+                    '<div class="sheet__user' + user_present + user_warning + user_warning_tooltip + '" id=user_' + user.userId + '>' +
                         '<div class="sheet__user__info">' +
                             '<div class="sheet__user__tick">' +
                                 '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 24.36"><path d="M32,6.36,29.45,3.82,10.18,23.09,2.54,15.45,0,18,10.18,28.18Z" transform="translate(0 -3.82)"/></svg>' +
@@ -54,24 +83,14 @@ $(document).on("ready", function () {
                         '<div class="sheet__user__icon">' +
                             '<img src="./assets/img/' + user.gender + '.svg" alt="male user">' +
                             '<div class="sheet__user__warning">' +
+                                '<span class="sheet__user__tooltip"><span>' + user_warning_tooltip__text + '</span></span>' +
                                 '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 19"><path d="M1,21H23L12,2Zm12-3H11V16h2Zm0-4H11V10h2Z" transform="translate(-1 -2)"/></svg>' +
                             '</div>' +
                         '</div>' +
                     '</div>';
                 sheet__users.append(content);
-                counter++;
-                if(user.present){
-                    counter_present++;
-                }
             });
-
-            var span_fraction = $(".sheet__summary__number__counter");
-            if(counter > 0) {
-                span_fraction.empty().text(counter_present + '/' + counter);
-            }
-
-            // console.log(counter);
-            // console.log(counter_present);
+            updateCounter();
         }
         
     });
